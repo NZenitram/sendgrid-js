181f460ddfb11196528d9bc5861912187f3700a3
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-05 09:20:44 -0600

    user create functional

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,35 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/v1/users', (req, res, next) => {
+  const results = [];
+  // Grab data from http request
+  const data = { username: req.body.username, email: req.body.email, password: req.body.password };
+   // Get a Postgres client from the connection pool
+   pg.connect(connectionString, (err, client, done) => {
+     // Handle connection errors
+     if(err) {
+       done();
+       console.log(err);
+       return res.status(500).json({success: false, data: err});
+     }
+     // SQL Query > Insert data
+     client.query('INSERT INTO users(username, email, password) values($1, $2, $3)',
+     [data.username, data.email, data.password]);
+     // SQL Query > Select data
+     const query = client.query('SELECT * FROM users ORDER BY id ASC');
+     // Stream results back one row at a time
+     query.on('row', (row) => {
+       results.push(row);
+     });
+     // After all data is returned, close connection and return results
+     query.on('end', () => {
+       done();
+       return res.json(results);
+     });
+   });
+});
+
+
+
 module.exports = router;
