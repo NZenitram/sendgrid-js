76a9d956bb1b92bbf06da7a79a8756c01187bda7
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-07 08:33:45 -0600

    floating navbar

--- public/javascripts/controllers/createUser.js
+++
@@ -1,8 +0,0 @@
-// var appRoutes = angular.module("sendgridWebhook", ["ngRoute"]);
-// appRoutes.config( ($routeProvider) => {
-//   $routeProvider
-//   .when("/createuser", {
-//     templateUrl: "createuser.html",
-//     controller: "userController"
-//   })
-// })
---
+++ public/javascripts/navbar/floatingNav.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  //change the integers below to match the height of your upper dive, which I called
+  //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+  //to figure out what the scroll position is when exactly you want to fix the nav
+  //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+  //you know the position.
+  $(window).scroll(function () {
+
+    console.log($(window).scrollTop());
+
+    if ($(window).scrollTop() > 150) {
+      $('#nav_bar').addClass('navbar-fixed-top');
+      $('#nav_bar').find("ul").removeClass('nav_links');
+
+    }
+
+    if ($(window).scrollTop() < 151) {
+      $('#nav_bar').removeClass('navbar-fixed-top');
+      $('#nav_bar').find("ul").addClass('nav_links');
+    }
+  });
+});
