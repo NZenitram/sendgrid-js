5dc7d474d254d3e29693396f287a7355af1c0d49
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-09 08:23:47 -0600

    testing users end point

---
+++ test/test-app.js
@@ -0,0 +1,7 @@
+describe('Users', function() {
+  it('should list ALL users on /uses GET');
+  it('should list a SINGLE usser on /blob/<id> GET');
+  it('should add a SINGLE blob on /blobs POST');
+  it('should update a SINGLE blob on /blob/<id> PUT');
+  it('should delete a SINGLE blob on /blob<id> DELETE');
+})
